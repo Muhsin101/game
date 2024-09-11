1b8e66e1c986e2fad7e3c3446f15f54f7ca20140
Muhsin Rahman <muhsin.rahman06@gmail.com>
2024-09-11 18:04:46 +0100

    added a hero through composition

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from enemy import *
 from zombie import *
 from ogre import *
+from hero import *
+from weapon import *
 
 def battle(e1: Enemy, e2: Enemy):
     e1.talk()
@@ -24,7 +26,32 @@ def battle(e1: Enemy, e2: Enemy):
     else:
         print(f"{e2.get_type_of_enemy()} wins!")
 
+def hero_battle(hero: Hero, enemy: Enemy):
+    enemy.talk()
+
+    while hero.health_points > 0 and enemy.health_points > 0:
+        print("----------")
+        enemy.special_attack()
+        print(f"Hero: {hero.health_points} HP left")
+        print(f"{enemy.get_type_of_enemy()}: {enemy.health_points} HP left")
+        enemy.attack()
+        hero.health_points -= enemy.attack_damage
+        hero.attack()
+        enemy.health_points -= hero.attack_damage
+
+    print("----------")
+
+    if hero.health_points > 0:
+        print(f"Hero wins!")
+    else:
+        print(f"{enemy.get_type_of_enemy()} wins!")
+
 zombie = (Zombie(10, 1))
 ogre = Ogre(20, 3)
+hero = Hero(10,1)
+weapon = Weapon("Sword", 5)
+hero.weapon = weapon
+hero.equip_weapon()
 
-battle(zombie, ogre)
+#battle(zombie, ogre)
+hero_battle(hero, zombie)
